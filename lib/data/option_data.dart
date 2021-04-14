64017a38a10ef7d93bf48a4cd6339ee2417642ce
engincanik <engincanik@gmail.com>
2021-04-14 12:16:48 +0300

    create data and models, add listview to homepage

---
+++ lib/data/option_data.dart
@@ -0,0 +1,7 @@
+import 'package:notify_me/models/condition_info.dart';
+
+List<ConditionInfo> conditions = [
+  ConditionInfo('Time', false),
+  ConditionInfo('Weather', false),
+  ConditionInfo('Location', false)
+];
